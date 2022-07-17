9b23ac818ceaee5cd11d4858dbf5570d6ad47dd3
brs <brv1114@yahoo.com>
2022-07-17 11:07:12 -0500

    building HTML file

--- index.js
+++ index.js
@@ -1,49 +1,167 @@
+//page links and packages 
 const inquirer = require('inquirer');
-//const path - require('path');
 const fs = require('fs');
 const Manager = require('./lib/manager');
 const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
-const Employee = require('./lib/employee');
-//const { isTypedArray } = require('util/types');
+//const Employee = require('./lib/employee');
+const generateHTML = require('./src/generateHTML');
 
-//information about and objects for team members
-//const teamInfo = [];
+const compTeamArray = [];
 
-// NEED to add job title somewhere
-
-//prompt question list
-inquirer 
-.prompt([
+//prompt question list start
+const addIntern = () => {
+    return inquirer.prompt ([
     {
-       type: "input",
+       type: "user input",
        name: "id",
-       message: "Please enter the Manager's ID" 
+       message: "Please enter your intern's ID" 
     },
     {
-        type: "input",
+        type: "user input",
         name: "name",
-        message: "Please enter the Manager's Name"
+        message: "Please enter your intern's full name"
     },
     {
-    type: "input",
+    type: "user input",
     name: "email",
-    message: "Please enter the Manager's Email Address",
+    message: "Please enter the intern's email address",
     },
     {
-    type: "input",
+    type: "user input",
     name: "office",
-    message: "Please enter the Manager's Office Number",
+    message: "Please enter the interns office number",
+},
+{
+    type: "user list",
+    name: "role",
+    message: "Please confirm the employee's company role is an Intern?",
+    choices: ["true, false"],
+},
+
+])
+.then(internInput => {
+    const {id, name, email, office } = internInput;
+    const intern = new Intern (id, name, email, office);
+
+    compTeamArray.push(intern);
+    console.log(intern);
+
+})
+};
+
+const newMember = () => {
+    console.log('Added Intern to the Team');
+
+    const addEngineer = () => {
+
+    return inquirer.prompt ([
+        {
+            type: "user input",
+            name: "id",
+            message: "Please enter the Engineer's company ID" 
+         },
+         {
+             type: "user input",
+             name: "name",
+             message: "Please enter the Engineer's full name"
+         },
+         {
+         type: "user input",
+         name: "email",
+         message: "Please enter the Engineer's email address",
+         },
+         {
+         type: "user input",
+         name: "office",
+         message: "Please enter the Engineer's office number",
+     },
+    ])}
+}
+.then(engineerInput => {
+    const {id, name, email, office } = engineerInput;
+    const engineer = new Engineer (id, name, email, office);
+
+    compTeamArray.push(engineer);
+    console.log(engineer);
+
+})
+
+const newMember = () => {
+    console.log('Added Engineer to the Team');
 }
 
-]);
+const addManager = () => {
 
-.then(answers => {
-    const manager = new ManagerInfo(answers.managerID, answers.managerName, answers.managerEmail, answers.managerOffice)
-    teamInfo.push(manager);
-    isTypedArray.push(answers.managerID);
-    fs.createReadStream();
+    return inquirer.prompt ([
+        {
+            type: "user input",
+            name: "id",
+            message: "Please enter the Manager's company ID" 
+         },
+         {
+             type: "user input",
+             name: "name",
+             message: "Please enter the Manager's full name"
+         },
+         {
+         type: "user input",
+         name: "email",
+         message: "Please enter the Manager's email address",
+         },
+         {
+         type: "user input",
+         name: "office",
+         message: "Please enter the Manager's office number",
+     },
+    ])}
+
+.then(managerInput => {
+    const {id, name, email, office } = managerInput;
+    const manager = new Manager (id, name, email, office);
+
+    compTeamArray.push(manager);
+    console.log(manager);
+
 })
+
+const newMember = () => {
+    console.log('Added Manager to the Team');
+}
+
+const {id, name, email, office, role } = employeeInfo;
+let employee;
+//end of questions array
+
+inquirer.prompt(createEmployee).then((ansers) => {
+    const newEmployee = new Employee(answer.id, answer.name, answer.email, answer.role)
+    employeeTeam.push(newEmployee);
+
+    createEmployeeTeam();
+});
+
+// create HTML file
+const writeFile = data => {
+    fs.writeFile('./dist/index.html');
+
+    }
+
+    addManager()
+    .then(addEmployee)
+    .then(teamArray => {
+        return generateHTML(teamArray);
+    })
+
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    });
+    
+
+
+
+//function call to initialize app
+init();
+
 
 
 
@@ -58,3 +176,4 @@ inquirer
 //resource: https://www.youtube.com/watch?v=bJY3KfM77pc
 //testing resource: https://threejs.org/docs/#manual/en/buildTools/Testing-with-NPM
 //info about Javascript super command: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/super
+//Inquirer Prompts: https://www.tabnine.com/code/javascript/functions/inquirer/Inquirer/prompt
---
+++ src/template.js
@@ -0,0 +1,135 @@
+//need to add font awesome icons
+//need to double check whether all src links are needed
+
+// Intern Card Layout
+
+const generateIntern = function (intern) {
+    return `
+    <div class="col-4 mt-4">
+    <div ckass="card h-100">
+    <div class ="card-header">
+        <h3>${intern.name}</h3>
+        <h4>Intern</h4><i class="NEED">NEED</i>
+    </div>
+    
+    <div class="card-body">
+        <p class="id"Emloyee ID: {intern.id}</p>
+        <p class="email">Email Address: <a href="mailto:${intern.email}">${intern.email}</a></p>
+        <p class="office">Office Number: ${intern.office}</p>
+    </div>
+</div>
+</div>
+`;
+}
+
+//Enginner Card Layout
+const generateEngineer = function (engineer) {
+    return `
+    <div class="card h-100">
+    <div class="card-header">
+    <h3>${engineer.name}</h3>
+    <h4>Engineer</h4><i class="NEED">NEED</i>
+</div>
+
+<div class="card-body">
+    <p class="id">Employee ID: ${engineer.id}</p>
+    <p class="email">Email Address: <a href="mailto:${engineer.email}">${enigineer.email}</a></p>
+    <p class="office">Office Number: ${engineer.office}<p>
+        </div>
+    </div>
+</div>
+`
+}
+
+//Manager Card Layout
+const generateManager = function (manager) {
+    return `
+    <div class="card h-100">
+    <div class="card-header">
+    <h3>${manager.name}</h3>
+    <h4>Team Manager</h4><i class="Need">NEED</i>
+</div>
+
+<div class="card-body">
+    <p class="id">Employee ID: ${manager.id}</p>
+    <p class="email">Email Address: <a href="mailto:${manager.email}">${manager.email}</a></p>
+    <p class="office">Office Number: ${manager.office}<p>
+        </div>
+    </div>
+</div>
+`
+}
+
+generateHTML=(data) => {
+    cardArray = [];
+
+    for (let i = 0; i < data.length; i++) {
+        const employee = data[i]
+        const role = employee.getRole();
+    }
+
+    //functions for each role
+    if (role === 'Intern') {
+        const cardIntern = generateIntern(employee);
+
+        cardArray.push(cardIntern);
+    }
+
+    if (role === 'Engineer') {
+        const cardEngineer = generateEngineer(employee);
+
+        cardArray.push(cardEngineer);
+    }
+
+    if (role === 'Manager') {
+        const cardManager = generateManager(employee);
+
+        cardArray.push(cardManager);
+    }
+}
+
+//join cards and array (strings)
+const teamCards = pageArray.join('')
+
+//return
+const generateTeamMembers = generatePage(teamCards);
+return generateTeamMembers;
+
+//create HTML page
+const generatePage = function (teamCards) {
+    return `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team-Profile</title>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar" id="nav">
+            <span class="navbar-brand mb-o h1 w-100 text-center" id="navbar-text">Team Profile Chart</span>
+        </nav>
+    </header>
+    <section>
+        <div class="container">
+            <div class="row justify-content-center" id="cards">
+            ${teamCards}
+        </div>
+    </div>
+</section>
+</body>
+</html>
+
+`;
+}
+
+
+module.exports = generateHTML;
